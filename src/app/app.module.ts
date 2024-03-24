84007e614ce54cb3ead10b2318e6c65d8e50db30
Juan David Arcila <j.arcila1@utp.edu.co>
2024-03-24 17:29:06 -0500

    intento de css by arcipapu jm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './plantillas/header/header.component';
 import { FooterComponent } from './plantillas/footer/footer.component';
 
 import {ReactiveFormsModule, FormsModule} from '@angular/forms'
@@ -12,7 +11,6 @@ import { HttpClientModule} from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     FooterComponent,
     routingComponents
   ],
